0e7af012769f8b5208592ef6837a4233c001de12
Hosung <an3735297@pusan.ac.kr>
2023-08-16 10:28:17 +0900

    Prog 교점에 별 만들기 / l2 / implementation, math
    https://school.programmers.co.kr/learn/courses/30/lessons/87377

---
+++ python/Prog_교점에별만들기.py
@@ -0,0 +1,35 @@
+def solution(line):
+    meet = []
+    for i in range(len(line)) :
+        for j in range(i+1, len(line)) :
+            u = (line[i][0]*line[j][2]) - (line[j][0]*line[i][2])
+            d = (line[j][0]*line[i][1]) - (line[i][0]*line[j][1])
+            if d == 0 : continue
+            y = u / d
+            if y != int(y) : continue
+            
+            u = (line[i][1]*line[j][2]) - (line[i][2]*line[j][1])
+            d = (line[i][0]*line[j][1]) - (line[i][1]*line[j][0])
+            if d == 0 : continue
+            x = u / d
+            if x != int(x) : continue
+            
+            meet.append([int(x), int(y)])
+            
+    meet.sort(key=lambda x : x[0])
+    X = meet[-1][0] - meet[0][0] + 1
+    sx = meet[0][0]
+    meet.sort(key=lambda x : x[1])
+    Y = meet[-1][0] - meet[0][0] + 1
+    sy = meet[-1][1]
+    
+    ans = []
+    for _ in range(Y) :
+        ans.append(['.']*X)
+        
+    for x,y in meet :
+        ans[sy-y][x - sx] = '*'
+    print(ans)
+                
+            
+            
