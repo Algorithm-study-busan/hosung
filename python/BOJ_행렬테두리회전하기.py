e5176e94ff0bb98ab933cf584495f79a352e11de
Hosung <an3735297@pusan.ac.kr>
2023-09-02 08:36:08 +0900

    BOJ 행렬 테두리 회전하기 / L2 / implementation
    https://school.programmers.co.kr/learn/courses/30/lessons/77485

---
+++ python/BOJ_행렬테두리회전하기.py
@@ -0,0 +1,43 @@
+def rotate(board, r1,c1, r2,c2) :
+    last = board[r1][c1]
+    ret = last
+    
+    for c in range(c1+1, c2+1) :
+        tmp = board[r1][c]
+        board[r1][c] = last
+        last = tmp
+        ret = min(ret, last)
+        
+    for r in range(r1+1, r2+1) :
+        tmp = board[r][c2]
+        board[r][c2] = last
+        last = tmp
+        ret = min(ret, tmp)
+        
+    for c in range(c2-1, c1-1, -1) :
+        tmp = board[r2][c]
+        board[r2][c] = last
+        last = tmp
+        ret = min(ret, tmp)
+        
+    for r in range(r2-1, r1-1, -1) :
+        tmp = board[r][c1]
+        board[r][c1] = last 
+        last = tmp
+        ret = min(ret, tmp)
+    
+    return ret
+    
+    
+    
+
+def solution(rows, columns, queries):
+    board = [[0 for _ in range(columns)] for _ in range(rows)]
+    for r in range(rows) :
+        for c in range(columns) :
+            board[r][c] = r*columns + c + 1
+    print(board)
+    print(rotate(board, 0,0, 3, 2))
+    print(board)
+    
+solution(4, 3, 3)
