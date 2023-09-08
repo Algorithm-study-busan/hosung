d66f4d63df9b5ab9c8eebaefac92fb82a6ae8c60
Hosung <an3735297@naver.com>
2023-09-08 10:59:59 +0900

    BOJ_13418 / G3 / MST
    https://www.acmicpc.net/problem/13418

---
+++ python/BOJ_13418.py
@@ -0,0 +1,39 @@
+edges = []
+V,E = map(int, input().split())
+SET = [i for i in range(V+1)]
+
+def get_parent(a) :
+    if a == SET[a] : return a
+    SET[a] = get_parent(SET[a])
+    return SET[a]
+
+def is_set(a, b) :
+    a = get_parent(a)
+    b = get_parent(b)
+    return a == b
+
+def union_node(a,b) :
+    a = get_parent(a)
+    b = get_parent(b)
+    if a < b : SET[b] = a
+    else : SET[a] = b
+    
+def MST() :
+    ret = 0
+    for a,b,c in edges :
+        if is_set(a,b) : continue
+        union_node(a,b)
+        ret += (c == 0)
+    return ret**2
+
+for _ in range(E+1) :
+    edges.append(list(map(int, input().split())))
+    
+edges.sort(key = lambda x : x[2])
+max_cost = MST()
+
+edges.sort(key = lambda x : -x[2])
+SET = [i for i in range(V+1)]
+min_cost = MST()
+
+print(max_cost - min_cost)
