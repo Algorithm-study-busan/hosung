c0e97e53d9981d43ea56a35ba8fdb61357769710
Hosung <an3735297@naver.com>
2023-09-11 10:43:53 +0900

    flat form class : 2주차 과제

---
+++ java/src/flatform/BooleanMain.java
@@ -0,0 +1,15 @@
+package flatform;
+
+import java.util.Scanner;
+
+public class BooleanMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println("Number1 : "+a+", Number2 : "+b);
+        System.out.println("Number1 > Number2 ? " + (a > b));
+        System.out.println("Number1 < Number2 ? " + (a < b));
+        System.out.println("Number1 == Number2 ? " + (a == b));
+    }
+}
---
+++ java/src/flatform/OperatorMain.java
@@ -0,0 +1,14 @@
+package flatform;
+
+import java.util.Scanner;
+
+public class OperatorMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.printf("%d / %d = %.3f%n", a,b, (float)a/b);
+        System.out.printf("%d = %d x %d + %d", a, a/b, b, a%b);
+    }
+}
