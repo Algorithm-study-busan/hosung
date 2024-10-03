db7d24106ccc449145e08f89b099de8655e74507
HOSUNG <an3735297@pusan.ac.kr>
2024-10-03 16:30:47 +0900

    LEET 577 / easy /sql - join

--- sql/LEET_577.sql
+++ sql/LEET_577.sql
@@ -1,4 +1,5 @@
-SELECT start.machine_id, ROUND(AVG(end.timestamp - start.timestamp), 3) processing_time
-FROM Activity start
-JOIN Activity end on start.machine_id = end.machine_id and start.process_id = end.process_id and end.activity_type = 'end' and start.activity_type = 'start'
-GROUP BY start.machine_id
+# Write your MySQL query statement below
+SELECT e.name, b.bonus
+FROM Employee e
+LEFT JOIN Bonus b on e.empId = b.empId
+WHERE b.bonus is null or b.bonus < 1000 
