cf4ae99fee8fbf3da36b4ae096af414d6d43b831
HOSUNG <an3735297@pusan.ac.kr>
2024-10-10 11:49:01 +0900

    LEET 1978 / easy / sub query

---
+++ sql/LEET_1978.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT employee_id
+FROM Employees
+WHERE 
+    salary < 30000 and
+    manager_id is not null and
+    manager_id not in (SELECT employee_id FROM Employees)
+ORDER BY employee_id
