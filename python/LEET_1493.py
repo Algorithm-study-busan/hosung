36ae77374fbe72ab52f17ac9770f7d25a26083ac
Hosung <an3735297@naver.com>
2025-01-21 17:40:23 +0900

    LEET 1493 / medium / sliding window

---
+++ python/LEET_1493.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        k = 1
+        start = 0
+        tmp = 0
+        ans = 0
+        for i,n in enumerate(nums) :
+            if n == 0 :
+                if k > 0 :
+                    tmp = i
+                    k -= 1
+                else :
+                    start = tmp+1
+                    tmp = i
+            ans = max(ans, i-start) 
+        return ans
+                
+            
+        
